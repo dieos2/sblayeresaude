30b8ba3144090084d60a920197f666f4abf3b1f7
Diego Oliveira Serra <dieos2@gmail.com>
2020-04-15 22:19:28 -0300

    interna de servico

--- views/servico/view.php
+++ views/servico/view.php
@@ -4,37 +4,46 @@ use yii\helpers\Html;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\Servico */
+/* @var $model app\models\Curso */
 
 $this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Servicos', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Cursos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="servico-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="wesley_tm_section" id="home" style="margin-top: 100px; background: #ffffff;">
+        <div class="wesley_tm_hero_header">
+            <div class="wesley_tm_universal_box_wrap">
+       <div class="container">
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+  <!-- Portfolio Item Heading -->
+  <h1 style="color: orange" class="my-4"><?= $model->titulo ?>
+      
+  </h1>
+  <h3 style="    margin-top: 10px;font-style: italic"> <strong><?= $model->subtitulo?></strong></h3>
+  <!-- Portfolio Item Row -->
+  <div class="row">
+ <div class="col-md-8">
+     
+      <?= $model->texto ?>
+       
+           </div>
+    <div class="col-md-4">
+        <img class="img-fluid" src="http://admin.sblaseresaude.com.br/images/uploads/<?= $model->foto?>" alt="">
+        
+       
+    </div>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'titulo',
-            'texto:ntext',
-            'id_user',
-            'data',
-            'status',
-        ],
-    ]) ?>
+   
+
+  </div>
+  <!-- /.row -->
+
+  <!-- Related Projects Row -->
+    <!-- /.row -->
 
 </div>
+<!-- /.container -->
+            </div>
+            </div>
+      </div>
