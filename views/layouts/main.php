1ece192a629cce62c7e692a4db56249daa77f247
Diego Oliveira Serra <dieos2@gmail.com>
2020-05-04 17:43:36 -0300

    ajuste de menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -109,9 +109,9 @@ HeadAsset::register($this);
 								<li class=""><a href="#services">Serviços</a></li>
                                                                 <li class=""><a href="#testimonials">Depoimentos</a></li>
                                                                 <li class=""><a href="#contact">Contato</a></li>
-                                                                <li class="" style="margin-right: 2px;"><a href="https://www.facebook.com/sb.laseresaude/" target="_blanck" class=""><img width="25" src="images/facebook.png"/></a></li>
+                                                                <li class="" style="margin-right: 2px;"><a href="https://www.facebook.com/sb.laseresaude/" target="_blank" class=""><img width="25" src="images/facebook.png"/></a></li>
                                                                 
-                                                                <li class=""><a href="https://www.instagram.com/saviobastos.sb" target="_blank" class=""><img width="25" src="images/instagram.png"/></a></li>
+                                                                <li class="" style="margin-right: 2px;"><a href="https://www.instagram.com/saviobastos.sb" target="_blank" class=""><img width="25" src="images/instagram.png"/></a></li>
                                                                  <li class=""><a href="https://api.whatsapp.com/send?phone=5591987027815" target="_blank" class=""><img width="25" src="images/whatzapp.png"/></a></li>
 								<!--<li class=""><a href="#portfolio">Agenda</a></li>-->
 								
